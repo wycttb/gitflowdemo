0439b04f5d13f60cba0f993bcaba2f2a74ebe283
YC W <ywu1988@outlook.com>
2020-02-03 00:19:51 -0500

    print cpu

--- gfdemo/profile.py
+++ gfdemo/profile.py
@@ -4,3 +4,7 @@ import socket
 hostname = socket.gethostname()
 print(hostname)
 print(socket.gethostbyname(hostname))
+import psutil
+# gives a single float value
+cpu = psutil.cpu_percent()
+print(cpu)
