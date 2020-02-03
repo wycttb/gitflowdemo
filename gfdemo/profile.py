e769e3bda38bbb2b7988ef8d35f1804880a220db
yuchen <yuchenwu9@yahoo.com>
2020-02-02 21:31:42 -0800

    merge"

--- gfdemo/profile.py
+++ gfdemo/profile.py
@@ -4,5 +4,12 @@ import socket
 hostname = socket.gethostname()
 print(hostname)
 print(socket.gethostbyname(hostname))
+<<<<<<< HEAD
 
 print('memory')
+=======
+import psutil
+# gives a single float value
+cpu = psutil.cpu_percent()
+print(cpu)
+>>>>>>> 0439b04f5d13f60cba0f993bcaba2f2a74ebe283
